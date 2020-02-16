e91a377040ef97abe141c2205de4871fc796999d
dantecoupet <48136583+dantecoupet@users.noreply.github.com>
2020-02-16 15:52:05 -0500

    Add files via upload

--- project/jingle/views.py
+++ project/jingle/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+posts = [
+    {'songName': 'yummy'},
+    {'songName': 'the box'},
+    {'songName': 'mo bamba'},
+    {'songName': 'sicko mode'}
+]
+
+def jingle_home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'jingle/index.html', context)
