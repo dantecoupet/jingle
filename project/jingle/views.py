2949558116449183157f442786d9f8d53f7f194c
JohnB116 <59739583+JohnB116@users.noreply.github.com>
2020-03-30 15:00:46 -0400

    Update views.py

--- project/jingle/views.py
+++ project/jingle/views.py
@@ -1,17 +1,78 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import requests
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from . import spotifyxx, genius
+from .forms import SongForm
 
-# Create your views here.
+def home(request):
+    if request.method == 'POST':
 
-posts = [
-    {'songName': 'yummy'},
-    {'songName': 'the box'},
-    {'songName': 'mo bamba'},
-    {'songName': 'sicko mode'}
-]
+        form = SongForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['songTitle']
+            form = SongForm()
 
-def jingle_home(request):
-    context = {
-        'posts': posts
-    }
-    return render(request, 'jingle/index.html', context)
+            songInfo = spotifyxx.find_song(text)
+            track = songInfo['name']
+            artist = songInfo['artists'][0]['name']
+            img = songInfo['album']['images'][0]['url']
+            albumName = songInfo['album']['name']
+            duration = songInfo['duration_ms']
+            duration = (duration/1000)/60
+            preview = songInfo['preview_url']
+
+            details = {
+                "artist":artist,
+                "track":track,
+                "imgSrc":img,
+                "album":albumName,
+                "duration":duration,
+                "preview":preview,
+            }
+
+            context = {
+                'song': details,
+                'form':form,
+            }
+            #return render(request,'jingle/results.html',context)
+            return render(request,'jingle/results.html',context)
+    else:
+        form = SongForm()
+
+    return render(request, 'jingle/home.html', {'form':form})
+
+def results(request):
+    if request.method == 'POST':
+
+        form = SongForm(request.POST)
+        if form.is_valid():
+            song = form.cleaned_data['songTitle']
+            form = SongForm()
+
+            songInfo = spotifyxx.find_song(song)
+            track = songInfo['name']
+            artist = songInfo['artists'][0]['name']
+            img = songInfo['album']['images'][0]['url']
+            albumName = songInfo['album']['name']
+            duration = songInfo['duration_ms']
+            duration = (duration/1000)/60
+            preview = songInfo['preview_url']
+
+            details = {
+                "artist":artist,
+                "track":track,
+                "imgSrc":img,
+                "album":albumName,
+                "duration":duration,
+                "preview":preview,
+            }
+
+            context = {
+                'song': details,
+                'form':form,
+            }
+
+            #return render(request,'jingle/results.html',context)
+            return render(request,'jingle/results.html',context)
+
+    return render(request,'jingle/results.html')
