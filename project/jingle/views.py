a70731e02385bee68610a8f9d0f9e6c0e232e618
JohnB116 <59739583+JohnB116@users.noreply.github.com>
2020-04-02 13:24:18 -0400

    Commit youtube additions

--- project/jingle/views.py
+++ project/jingle/views.py
@@ -29,10 +29,51 @@ def home(request):
                 "duration":duration,
                 "preview":preview,
             }
+            
+            search_params = {
+                'part': 'snippet',
+                'q': track + ' ' + artist,
+                'key': 'AIzaSyD-ALkXytH1CKrsYSyzTSVTzt8c3PeazM4',
+                'maxResults': 3,
+                'type': 'video'
+            }
+
+            #get information from youtube api
+            video_ids = []
+            videos = []
+            search_url = 'https://www.googleapis.com/youtube/v3/search'
+            video_url = 'https://www.googleapis.com/youtube/v3/videos'
+            vid = requests.get(search_url, params=search_params)
+            results_v = vid.json()['items']
+
+            for result in results_v:
+                video_ids.append(result['id']['videoId'])
+
+            video_params = {
+                'key': 'AIzaSyD-ALkXytH1CKrsYSyzTSVTzt8c3PeazM4',
+                'part': 'snippet, contentDetails',
+                'id': ','.join(video_ids)
+            }
+
+            vid2 = requests.get(video_url, params=video_params)
+
+            results_t = vid2.json()['items']
+
+            for result in results_t:
+                video_data = {
+                    'title': result['snippet']['title'],
+                    'id': result['id'],
+                    'url': f'https://www.youtube.com/watch?v={result["id"]}',
+                    'thumbnail': result['snippet']['thumbnails']['high']['url']
+                }
+
+                videos.append(video_data)
+
 
             context = {
                 'song': details,
                 'form':form,
+                'videos':videos
             }
             #return render(request,'jingle/results.html',context)
             return render(request,'jingle/results.html',context)
@@ -66,10 +107,51 @@ def results(request):
                 "duration":duration,
                 "preview":preview,
             }
+            
+            search_params = {
+                'part': 'snippet',
+                'q': track + ' ' + artist,
+                'key': 'AIzaSyD-ALkXytH1CKrsYSyzTSVTzt8c3PeazM4',
+                'maxResults': 3,
+                'type': 'video'
+            }
+
+            #get information from youtube api
+            video_ids = []
+            videos = []
+            search_url = 'https://www.googleapis.com/youtube/v3/search'
+            video_url = 'https://www.googleapis.com/youtube/v3/videos'
+            vid = requests.get(search_url, params=search_params)
+            results_v = vid.json()['items']
+
+            for result in results_v:
+                video_ids.append(result['id']['videoId'])
+
+            video_params = {
+                'key': 'AIzaSyD-ALkXytH1CKrsYSyzTSVTzt8c3PeazM4',
+                'part': 'snippet, contentDetails',
+                'id': ','.join(video_ids)
+            }
+
+            vid2 = requests.get(video_url, params=video_params)
+
+            results_t = vid2.json()['items']
+
+            for result in results_t:
+                video_data = {
+                    'title': result['snippet']['title'],
+                    'id': result['id'],
+                    'url': f'https://www.youtube.com/watch?v={result["id"]}',
+                    'thumbnail': result['snippet']['thumbnails']['high']['url']
+                }
+
+                videos.append(video_data)
+
 
             context = {
                 'song': details,
                 'form':form,
+                'videos', videos
             }
 
             #return render(request,'jingle/results.html',context)
