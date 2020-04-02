bc793a022e3ac361f6966cc4cdd3038d1c9d2a4a
JohnB116 <59739583+JohnB116@users.noreply.github.com>
2020-04-02 18:00:54 -0400

    Update urls.py

--- project/jingle/urls.py
+++ project/jingle/urls.py
@@ -4,8 +4,7 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    path('', views.home,name='home'),
-    path('results/', views.results,name='results'),
+    path('', views.jingle_home,name='jingle-home'),
+    path('results/<slug:song>/', views.jingle_top_search,name='jingle-results'),
+    path('song/<slug:spotify_id>/', views.jingle_results,name='jingle-song'),
 ]
-
-
