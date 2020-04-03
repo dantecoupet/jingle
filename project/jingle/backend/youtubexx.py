11a43679273b934a9a9d7c8325a5f4afd4d1188b
JohnB116 <59739583+JohnB116@users.noreply.github.com>
2020-04-03 17:31:33 -0400

    Update youtube api key

--- project/jingle/backend/youtubexx.py
+++ project/jingle/backend/youtubexx.py
@@ -4,7 +4,7 @@ def get_youtube(song, artist):
 	search_params = {
 		'part': 'snippet',
 		'q': song + ' ' + artist,
-		'key': 'AIzaSyD-ALkXytH1CKrsYSyzTSVTzt8c3PeazM4',
+		'key': 'AIzaSyDeumt0L9rYG89HZmc7bXU4hY1wgAG0Vho',
 		'maxResults': 3,
 		'type': 'video'
 	}
@@ -21,7 +21,7 @@ def get_youtube(song, artist):
 		video_ids.append(result['id']['videoId'])
 
 	video_params = {
-		'key': 'AIzaSyD-ALkXytH1CKrsYSyzTSVTzt8c3PeazM4',
+		'key': 'AIzaSyDeumt0L9rYG89HZmc7bXU4hY1wgAG0Vho',
 		'part': 'snippet, contentDetails',
 		'id': ','.join(video_ids)
 	}
