3a82aa21af3c96a93a9721ff503a31a05849e016
jackgolfer <61120119+jackgolfer@users.noreply.github.com>
2020-04-24 17:38:30 -0400

    Update urls.py

--- Iteration3/SourceCode/jingle/urls.py
+++ Iteration3/SourceCode/jingle/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.jingle_home,name='jingle-home'),
-    path('results/<slug:song>/', views.jingle_top_search,name='jingle-results'),
+    path('results/<str:song>/', views.jingle_top_search,name='jingle-results'),
     path('song/<slug:spotify_id>/', views.jingle_results,name='jingle-song'),
     path('feedback/', views.jingle_feedback,name='jingle-feedback'),
     path('top_results/', views.jingle_topfifty,name='jingle-topfifty'),
