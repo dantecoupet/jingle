def646c1e19f21fd4a874c5ebad4c6abaeaa1cc6
gjrocky <49589879+gjrocky@users.noreply.github.com>
2020-04-24 14:38:52 -0400

    Add files via upload

---
+++ project/jingle/backend/master_results.py
@@ -0,0 +1,79 @@
+from . import spotifyxx, geniusxx, youtubexx
+
+
+#master dictionary that will be returned
+masterDict = {
+		#spotify API
+		"artist_name":"",
+		"song_name":"",
+		"album_name":"",
+		"song_cover_url":"",
+		"song_duration":"",
+		"artist_additional_names":"",
+		"release_date":"",
+		"spotify_song_link":"",
+		"genres":"",
+		"song_name_short":"",
+		"preview":"",
+		#genius API
+		"lyrics":"",
+		"genius_page_views":"",
+		"genius_song_link":"",
+		#youtube API
+		"videos":"",
+		"errors":""
+		
+	}
+
+fiftydict = {
+
+	"song_title":"",
+	"artist_name":"",
+}
+
+
+#master function that calls all the sub functions from other files
+def get_master(name):
+	
+	#calls the spotify function and passes in name of song
+	spotifyResults = spotifyxx.return_song(name)
+	masterDict["artist_name"] = spotifyResults["artist_name"]
+	masterDict["song_name"] = spotifyResults["song_name"]
+	masterDict["album_name"] = spotifyResults["album_name"]
+	masterDict["song_cover_url"] = spotifyResults["song_cover_url"]
+	masterDict["song_duration"] = spotifyResults["song_duration"]
+	masterDict["artist_additional_names"] = spotifyResults["artist_additional_names"]
+	masterDict["release_date"] = spotifyResults["release_date"]
+	masterDict["spotify_song_link"] = spotifyResults["spotify_song_link"]
+	masterDict["genres"] = spotifyResults["genres"]
+	masterDict["song_name_short"] = spotifyResults["song_name_short"]
+	masterDict["preview"] = spotifyResults["preview"]
+	masterDict["errors"] = spotifyResults["errors"]
+	
+	#calls the genius function, passing in song/artist name from spotify
+	if masterDict["errors"] == "":
+		geniusResults = geniusxx.genius_results(masterDict["song_name_short"], masterDict["artist_name"])
+		masterDict["lyrics"] = geniusResults["lyrics"]
+		masterDict["genius_page_views"] = geniusResults["genius_page_views"]
+		masterDict["genius_song_link"] = geniusResults["genius_song_link"]
+		masterDict["errors"] = geniusResults["errors"]
+		
+		youtubeResults = youtubexx.get_youtube(masterDict["song_name_short"], masterDict["artist_name"])
+		masterDict["videos"] = youtubeResults
+
+	return masterDict
+	
+def get_top_search(name):
+	return spotifyxx.spotify_top_search(name)
+
+def top_fifty():
+
+	topresults = spotifyxx.get_top_fifty()
+	fiftydict["song_title"] = topresults['name']
+	fiftydict["artist_name"] = topresults['artist']
+
+	return fiftydict
+
+
+	
+	
