e426a906edf8e8136cee47a62e673b2552f8b95a
jackgolfer <61120119+jackgolfer@users.noreply.github.com>
2020-04-24 17:05:15 -0400

    Add files via upload

--- Iteration3/SourceCode/jingle/backend/youtubexx.py
+++ Iteration3/SourceCode/jingle/backend/youtubexx.py
@@ -4,7 +4,7 @@ def get_youtube(song, artist):
 	search_params = {
 		'part': 'snippet',
 		'q': song + ' ' + artist,
-		'key': 'AIzaSyDGTZ8yzIv8qiIJ8CuBBbKkIMtarZ5S8GM',
+		'key': 'AIzaSyDeumt0L9rYG89HZmc7bXU4hY1wgAG0Vho',
 		'maxResults': 3,
 		'type': 'video'
 	}
@@ -15,19 +15,25 @@ def get_youtube(song, artist):
 	search_url = 'https://www.googleapis.com/youtube/v3/search'
 	video_url = 'https://www.googleapis.com/youtube/v3/videos'
 	vid = requests.get(search_url, params=search_params)
+	error = vid.json().keys()
+	indexList = list(error)
+
+	for i in indexList:
+		if(i == "error"):
+			return videos
+
 	results_v = vid.json()['items']
 
 	for result in results_v:
 		video_ids.append(result['id']['videoId'])
 
 	video_params = {
-		'key': 'AIzaSyDGTZ8yzIv8qiIJ8CuBBbKkIMtarZ5S8GM',
+		'key': 'AIzaSyDeumt0L9rYG89HZmc7bXU4hY1wgAG0Vho',
 		'part': 'snippet, contentDetails',
 		'id': ','.join(video_ids)
 	}
 
 	vid2 = requests.get(video_url, params=video_params)
-
 	results_t = vid2.json()['items']
 
 	for result in results_t:
