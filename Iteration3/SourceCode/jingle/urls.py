e53d55defdedffa1f7ac5c7660ebf6d1e40c15fa
gjrocky <49589879+gjrocky@users.noreply.github.com>
2020-04-24 16:55:03 -0400

    Add files via upload

--- Iteration3/SourceCode/jingle/urls.py
+++ Iteration3/SourceCode/jingle/urls.py
@@ -6,8 +6,7 @@ urlpatterns = [
     path('results/<slug:song>/', views.jingle_top_search,name='jingle-results'),
     path('song/<slug:spotify_id>/', views.jingle_results,name='jingle-song'),
     path('feedback/', views.jinge_feedback,name='jingle-feedback'),
-    path('about/', views.jingle_about,name='jingle-about'),
-    #path('feedback/thanks/', views.jinge_thanks,name='jingle-thanks'),
-    #path('about/', views.jinge_about,name='jingle-about'),
+    path('topfifty/', views.jinge_topfifty,name='jingle-topfifty'),
+    #path('topfifty/<slug:song>/', views.jingle_results,name='jingle-results'),
     #path('error', views.jinge_error,name='jingle-error'),
 ]
