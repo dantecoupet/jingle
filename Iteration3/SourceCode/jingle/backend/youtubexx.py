c29d25e2e067008f3a628a173a1baa82f9d98216
JohnB116 <59739583+JohnB116@users.noreply.github.com>
2020-04-24 16:46:20 -0400

    Add files via upload

---
+++ Iteration3/SourceCode/jingle/backend/youtubexx.py
@@ -0,0 +1,47 @@
+import requests
+
+def get_youtube(song, artist):
+	search_params = {
+		'part': 'snippet',
+		'q': song + ' ' + artist,
+		'key': 'AIzaSyDGTZ8yzIv8qiIJ8CuBBbKkIMtarZ5S8GM',
+		'maxResults': 3,
+		'type': 'video'
+	}
+
+	#get information from youtube api
+	video_ids = []
+	videos = []
+	search_url = 'https://www.googleapis.com/youtube/v3/search'
+	video_url = 'https://www.googleapis.com/youtube/v3/videos'
+	vid = requests.get(search_url, params=search_params)
+	if vid.json()['error'] != ():
+		return videos
+	results_v = vid.json()['items']
+
+	for result in results_v:
+		video_ids.append(result['id']['videoId'])
+
+	video_params = {
+		'key': 'AIzaSyDGTZ8yzIv8qiIJ8CuBBbKkIMtarZ5S8GM',
+		'part': 'snippet, contentDetails',
+		'id': ','.join(video_ids)
+	}
+
+	vid2 = requests.get(video_url, params=video_params)
+
+	results_t = vid2.json()['items']
+
+	for result in results_t:
+		video_data = {
+			'title': result['snippet']['title'],
+			'id': result['id'],
+			'url': f'https://www.youtube.com/watch?v={result["id"]}',
+			'thumbnail': result['snippet']['thumbnails']['high']['url']
+			}
+
+		videos.append(video_data)
+
+	return videos
+
+
